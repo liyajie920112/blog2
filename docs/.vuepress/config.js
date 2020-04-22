a2b7c62750b8c257861462f44c6cea3b9c461354
liyajie <liyajie920112@gmail.com>
2020-04-23 00:37:06 +0800

    add side

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,21 @@
 module.exports = {
+  title: 'LiYajie',
+  description: 'LiYajie技术小栈',
   themeConfig: {
     sidebar: 'auto',
     nav: [{
       text: 'Home', link: '/'
     }, {
       text: 'Blog', link: '/blog/'
+    }],
+    sidebar: [{
+      title: '面试',
+      // path: '/blog/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+      collapsable: true, // 可选的, 默认值是 true,
+      sidebarDepth: 1,    // 可选的, 默认值是 1
+      children: [
+        '/blog/interview/'
+      ]
     }],
     smoothScroll: true
   }
