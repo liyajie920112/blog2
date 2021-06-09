2e5135bb88911fda75127acba73c5e745d7838f9
liyj <liyj@tsingyun.net>
2021-06-09 09:44:58 +0800

    feat: add lastUpdated date

--- docs/.vuepress/theme/components/NavHeader.vue
+++ docs/.vuepress/theme/components/NavHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="nav-header">
     <ul>
-      <li v-for="item in pages" :key="item.key"><a :href="item.path" >{{ item.title }}</a></li>
+      <li v-for="item in pages" :key="item.key"><a :href="item.path" >[{{ item.lastUpdated }}] {{ item.title }}</a></li>
     </ul>
   </div>
 </template>
@@ -13,7 +13,11 @@ export default {
       return this.$page
     },
     pages() {
-      return this.$site.pages.filter(a => a.path.startsWith(this.page.path) && a.path !== this.page.path)
+      const pages = this.$site.pages.filter(a => a.path.startsWith(this.page.path) && a.path !== this.page.path)
+      pages.sort((a, b) => {
+        return new Date(b.lastUpdated).getTime() - new Date(a.lastUpdated).getTime()
+      })
+      return pages
     }
   }
 }
